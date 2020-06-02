76ff3b8983db1a7734021820a75ee855880d5659
melanie Boardman <melanie_boardman@hotmail.com>
2020-06-02 23:14:08 +0100

    added data to all_recipes.html to display information for recipe thumbnails

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
+from os import path
+if path.exists("env.py"):
+  import env
 
+app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'bake-it'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-b7vrk.mongodb.net/bakeIt?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
-    return render_template("all_recipes.html", recipes=mongo.db.recipes.find(), recipe_category=mongo.db.recipe_category.find())
+    return render_template("all_recipes.html", cat_list = list(mongo.db.recipe_category.find()), recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
