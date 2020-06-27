49a3daaec66988b881b27ce2bead967f9e40ef67
melanie Boardman <melanie_boardman@hotmail.com>
2020-06-27 22:26:01 +0100

    Added delete functionality

--- app.py
+++ app.py
@@ -122,6 +122,11 @@ def update_recipe(recipe_id):
     })
   return redirect(url_for('get_recipes'))
 
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
